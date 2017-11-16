05bc4758146a4f49e5ed982b5915d506975ecc71
richard2104 <richard2104a@gmail.com>
2017-11-16 11:13:04 -0800

    what's the problem dude... too many values to unpack

--- dot_split.py
+++ dot_split.py
@@ -2,24 +2,26 @@ import os
 
 # I used domain tail(?) such as .com .kr .jp .fr etc
 f = open('top-1m.csv')
-
+directory = './dot_list/'
 char_set = []
 dot = ''
 count_dot = 0
 put_url = {}
 count = 0
+os.system('mkdir -p dot_list')
+
 while True:
 	line = f.readline()
-	if not line: break
-	count = count +1
-	
+	if not line:
+		break
+	count = count + 1
 	line = line.split(',')[1] # removing index
 	char_set = line.split('.')
 	dot = char_set[len(char_set)-1]
 	dot = dot[:-1] # erasing \n
 
-	print char_set
-	print dot # .com / .kr / etc
+	#print char_set
+	#print dot # .com / .kr / etc
 	
 	# dictionary ex) .com : www.naver.com
 	if (dot in put_url.keys()): # .keys() helps to print out all the keys in put_url dict.
@@ -29,12 +31,17 @@ while True:
 	else:
 		put_url[dot] = []
 		put_url[dot].append(line[:-1])
-	if count == 10: break
+	if (count % 10000 == 0):
+		print count
 
+print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+for key,value in put_url:	# value is list!
+	print key,value
+	f2 = open(directory + key + '.txt', 'w')
+	for i in range(len(value)):
+		f2.write(value[i]+'\n')
+	f2.close()
+	
 
-print put_url
-
-# csv file 
-#os.system("cat top-1m.csv | grep 'www' > sorted1.csv")
+#print put_url
 f.close()
-
